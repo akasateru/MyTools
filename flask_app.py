13c452f0bd1f9aba6111d17e11285cae7ea87d70
akasateru <akasateru@gmail.com>
2023-03-24 11:33:45 +0900

    first commit

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,4 @@
-from flask import Flask, url_for, redirect, render_template
-from jinja2 import Template
+from flask import Flask, render_template
 import datetime
 import csv
 import itertools
@@ -14,10 +13,10 @@ def hello_world():
   for i in range(20):
     return render_template("top.html",i=i)
 
-@app.route('/count_down_timer')
+@app.route('/pomodoro')
 def projects():
   today =  datetime.date.today()
-  return render_template("count_down_timer.html",today=today)
+  return render_template("pomodoro.html",today=today)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
